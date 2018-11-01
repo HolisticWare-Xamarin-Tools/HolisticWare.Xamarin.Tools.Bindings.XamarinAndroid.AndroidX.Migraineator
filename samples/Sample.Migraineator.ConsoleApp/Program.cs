60afb30290a8be6479071f25429772fdc7799b8c
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-11-01 22:02:23 +0100

    sample console app for migrating Metadata.xml

--- samples/Sample.Migraineator.ConsoleApp/Program.cs
+++ samples/Sample.Migraineator.ConsoleApp/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.AndroidX.Migraineator.Core;
 
 namespace Sample.Migraineator.ConsoleApp
 {
     class Program
     {
         static int verbosity;
+        static AndroidXMigrator androidx_migrator = null;
 
         public static void Main(string[] args)
         {
+            androidx_migrator = new AndroidXMigrator();
+
             bool show_help = false;
             List<string> names = new List<string>();
             string folder_input = null;
@@ -80,16 +84,17 @@ namespace Sample.Migraineator.ConsoleApp
                 Debug($"Using default message: {message}");
             }
 
-            if (string.IsNullOrWhiteSpace(folder_input))
-            {
-                folder_input = 
-                    @"../../../../X/AndroidSupportComponents-AndroidX-binderate/source/androidx.core/"
-                    //@"../../../../X/AndroidSupportComponents-AndroidX-binderate/source/androidx.core/"
-                    ;
-            }
             if (string.IsNullOrWhiteSpace(folder_output))
             {
                 folder_output = "../../../dumps/";
+            }
+            if (string.IsNullOrWhiteSpace(folder_input))
+            {
+                folder_input =
+                    @"../../../../X/AndroidSupportComponents-AndroidX-binderate/source/"
+                    //@"../../../../X/AndroidSupportComponents-AndroidX-binderate/source/androidx.core/"
+                    //@"../../../../X/AndroidSupportComponents-AndroidX-binderate/source/androidx.core/"
+                    ;
             }
 
             Console.WriteLine($"Migrating Android.Support in:");
@@ -97,34 +102,104 @@ namespace Sample.Migraineator.ConsoleApp
             Console.WriteLine($"  to  AndroidX in:");
             Console.WriteLine($"    {folder_output}");
 
-            Task t = await ProcessMetadataFilesAsync(folder_input, folder_output);
+            Task t = ProcessMetadataFilesAsync(folder_input, folder_output);
+
+            Task.WaitAll(t);
 
             return;
         }
 
         private static async Task ProcessMetadataFilesAsync(string folder_input, string folder_output)
         {
+            #if DEBUG && NETCOREAPP
+             await androidx_migrator.InitializeAsync("./bin/Debug/netcoreapp2.1/mappings/");
+#else
+            androidx_migrator.Initialize("./mappings/");
+#endif
+
+            Console.WriteLine($"Migrating files: ");
             string[] files = System.IO.Directory.GetFiles
                                                         (
-                                                            folder_input, 
-                                                            "Metadata*.xml", 
+                                                            folder_input,
+                                                            "Metadata*.xml",
                                                             System.IO.SearchOption.AllDirectories
                                                         );
 
-            Console.WriteLine($"Migrating files: ");
-            for (int i = 0; i < files.Length; i++)
-            {
-                Console.WriteLine($"    {files[i]}");
+            await MigrateFilesMedtadataSequentialAsync(files);
 
-                using (var stream = new System.IO.FileStream(@"C:\temp\test.txt", System.IO.FileMode.Open))
-                using (var reader = new System.IO.StreamReader(stream))
-                {
-                    string content =  await reader.ReadToEndAsync();
-                }
-            }
+            //MigrateFilesParallel(files);
 
 
             return;
+        }
+
+        private static async Task MigrateFilesMedtadataSequentialAsync(string[] files)
+        {
+            for (int i = 0; i < files.Length; i++)
+            {
+                string file = files[i];
+                Console.WriteLine($"    {file}");
+
+                try
+                {
+                    string content_migrated = await androidx_migrator.MigrateMetadataFileNamespacesAsync(file);
+
+                    //System.IO.File.WriteAllText(file, content_migrated);
+                    System.IO.File.WriteAllText($"{file}", content_migrated);
+                }
+                catch (AggregateException exc)
+                {
+                    Console.WriteLine($"AndroidXMigrator Exception {exc.Message}");
+
+                    throw;
+                }
+            }
+
+            return;
+        }
+
+        private static void MigrateFilesParallel(string[] files)
+        {
+            Parallel.For
+                    (
+                        0,
+                        files.Length,
+                        async i =>
+                        {
+                            string file = files[i];
+                            Console.WriteLine($"    {file}");
+
+                            try
+                            {
+                                string content = null;
+                                using
+                                    (
+                                        System.IO.FileStream stream = System.IO.File.Open
+                                                                                        (
+                                                                                            file,
+                                                                                            System.IO.FileMode.Open,
+                                                                                            System.IO.FileAccess.Read
+                                                                                        )
+                                    )
+                                using
+                                    (
+                                        System.IO.TextReader reader = new System.IO.StreamReader(stream)
+                                    )
+                                {
+                                    content = await reader.ReadToEndAsync();
+                                }
+                                //string content = System.IO.File.ReadAllText(file);
+                                string content_migrated = await androidx_migrator.MigrateMetadataFileNamespacesAsync(content);
+                                //System.IO.File.WriteAllText(file, content_migrated);
+                            }
+                            catch (AggregateException exc)
+                            {
+                                Console.WriteLine($"AndroidXMigrator Exception {exc.Message}");
+
+                                throw;
+                            }
+                        }
+                    );
         }
 
         static void ShowHelp(Mono.Options.OptionSet p)
