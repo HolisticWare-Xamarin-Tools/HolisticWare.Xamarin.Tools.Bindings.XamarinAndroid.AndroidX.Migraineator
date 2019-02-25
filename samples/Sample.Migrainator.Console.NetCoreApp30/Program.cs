5d5dcc980161c9ce2bd55113d90f1ba9f8315eaa
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-02-25 16:52:50 +0100

    source split into Migrainator (miration) and MigraineDiagnoser (analysis)

---
+++ samples/Sample.Migrainator.Console.NetCoreApp30/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sample.Migrainator
+{
+    class Program
+    {
+        static int verbosity;
+        static AndroidXMigrator androidx_migrator = null;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
