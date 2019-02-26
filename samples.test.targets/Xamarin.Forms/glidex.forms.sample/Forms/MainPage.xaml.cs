aea0eb414e0938c4d42cdbaca9386bfab7cb9ed3
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-02-26 22:51:18 +0100

    test target samples

---
+++ samples.test.targets/Xamarin.Forms/glidex.forms.sample/Forms/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Android.Glide.Sample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+		async void Grid_Clicked (object sender, EventArgs e)
+		{
+			await Navigation.PushAsync (new GridPage ());
+		}
+		async void Edge_Clicked (object sender, EventArgs e)
+		{
+			await Navigation.PushAsync (new EdgeCasesPage ());
+		}
+
+		async void ViewCell_Clicked (object sender, EventArgs e)
+		{
+			await Navigation.PushAsync (new ViewCellPage ());
+		}
+
+		async void ImageCell_Clicked (object sender, EventArgs e)
+		{
+			await Navigation.PushAsync (new ImageCellPage ());
+		}
+
+		async void HugeImage_Clicked (object sender, EventArgs e)
+		{
+			await Navigation.PushAsync (new HugeImagePage ());
+		}
+
+		async void ToggleImages_Clicked (object sender, EventArgs e)
+		{
+			await Navigation.PushAsync (new ToggleSourcePage ());
+		}
+
+		async void ToggleImagesMaterial_Clicked (object sender, EventArgs e)
+		{
+			await Navigation.PushAsync (new ToggleSourcePage { Visual = VisualMarker.Material });
+		}
+	}
+}
