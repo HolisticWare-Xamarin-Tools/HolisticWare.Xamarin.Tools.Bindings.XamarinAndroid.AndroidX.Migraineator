56a6276c9839279b92ce9ac142abc8877aa028f2
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-03-14 23:54:54 +0100

    samples refactoring and update

---
+++ samples/App.Xamarin.AndroidX.Mapper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.AndroidX.Mapper;
+
+namespace App.Xamarin.AndroidX.Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string classes = MappingsGoogle.MappingsRawClasses;
+
+            return;
+        }
+    }
+}
