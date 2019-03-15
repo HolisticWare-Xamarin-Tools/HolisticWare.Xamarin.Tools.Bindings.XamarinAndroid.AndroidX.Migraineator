6bbc718f02f46ed9e30f5108b86a11ced54bb98f
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-03-15 17:00:49 +0100

    Mapper sample

--- samples/App.Xamarin.AndroidX.Mapper/Program.cs
+++ samples/App.Xamarin.AndroidX.Mapper/Program.cs
@@ -1,13 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.AndroidX.Data;
 using Xamarin.AndroidX.Mapper;
 
 namespace App.Xamarin.AndroidX.Mapper
 {
     class Program
     {
+        static string url_base = "https://github.com/moljac/HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.AndroidX.Data/raw/master/data/";
+
         static void Main(string[] args)
         {
-            string classes = MappingsGoogle.MappingsRawClasses;
+
+            MappingsXamarin xamarin_android_support = new MappingsXamarin();
+            xamarin_android_support.Download
+                                    (
+                                        "Android.Support.merged",
+                                        $"{url_base}Android.Support/AndroidSupport.Merged.dll"
+                                    );
+
+            MappingsXamarin xamarin_androidx = new MappingsXamarin();
+            xamarin_androidx.Download
+                                    (
+                                        "AndroidX.merged",
+                                        $"{url_base}AndroidX/AndroidSupport.Merged.dll"
+                                    );
+
+            // various Data objects will prepare Collections for faster search
+
+            DataOptimizedSortedOnly data_optimized_sorted = new DataOptimizedSortedOnly();
+            // parse CSV - google's mappings
+            data_optimized_sorted.DataTable = MappingsGoogle.Parse();
+            // initialize all objects for search
+            data_optimized_sorted.Initialize();
+
+
+            DataOptimizedSortedSharded data_optimized_sorted_sharded = new DataOptimizedSortedSharded();
+            // parse CSV - google's mappings
+            data_optimized_sorted_sharded.DataTable = MappingsGoogle.Parse();
+            // initialize all objects for search
+            data_optimized_sorted_sharded.Initialize();
+            data_optimized_sorted_sharded.Analyze();
+
+
+
 
             return;
         }
